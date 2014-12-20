83e0f637be0ed0c5c169afd07e713daa8dbc2b33
SunDwarf <eyesismine@gmail.com>
2014-12-20 23:23:32 +0000

    Fixed byte download display

--- pkgman/ProgressBar.py
+++ pkgman/ProgressBar.py
@@ -3,21 +3,23 @@ import requests
 from progressbar import Bar, FileTransferSpeed, Percentage, ProgressBar
 # Taken from http://stackoverflow.com/questions/10525635/python-3-and-requests-with-a-progressbar
 def get_file(file_url, name):
-    r = requests.get(file_url)
+    r = requests.get(file_url, stream=True)
     if not r.status_code == requests.codes.ok:
         print("Getting URL " + file_url + " failed ({code})".format(code=r.status_code), file=sys.stderr)
         return None
     size = int(r.headers['Content-Length'].strip())
     bytes = 0
-    widgets = [name, ": ", Bar(marker="=", left="[", right=""),
-        "] ", Percentage(), " ",  FileTransferSpeed(), " ",
-        "- {0}B".format(str(round(size, 4))[:4])]
+    widgets = [name, ": ", Bar(marker="=", left="[", right="] "),
+        Percentage(), " ",  FileTransferSpeed(), " ",
+        str(round(bytes / 1024, 2)), "KB"
+        " of {0}KB".format(str(round(size / 1024, 2)))]
     pbar = ProgressBar(widgets=widgets, maxval=size, fd=sys.stdout).start()
     file = []
     for buf in r.iter_content(1024):
         if buf:
             file.append(buf)
             bytes += len(buf)
+            pbar.widgets[7] = str(round(bytes / 1024, 2))
             pbar.update(bytes)
     pbar.finish()
     return b"".join(file)
