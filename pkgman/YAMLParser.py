92b95ffa7b26fdedba10e423fd1779ba6b59762e
SunDwarf <eyesismine@gmail.com>
2014-12-20 20:39:23 +0000

    Add sample config file and configuration access to YAMLParser

--- pkgman/YAMLParser.py
+++ pkgman/YAMLParser.py
@@ -1,7 +1,10 @@
 from . import FileHelper
+
+
 class YAMLBase(object):
     def __init__(self):
         pass
+
 
 class Repo(YAMLBase):
     def __init__(self, file):
@@ -20,5 +23,60 @@ class Repo(YAMLBase):
             }
             FileHelper.WriteYAMLFile('var/wpkgman/repos' + file + '.yml', content)
         else:
-            self.name = yaml_output['name']
+            self.name = yaml_output['repo']
             self.priority = yaml_output['priority']
+
+class Config(YAMLBase):
+    def __init__(self):
+        super().__init__()
+
+        self.options = None
+
+        # Auto parse the repo file, and fill in the fields
+        yaml_output = FileHelper.OpenYAMLFile('etc/wpkgman.yml')
+        # TODO: Add auto downloading of the repo files if possible.
+        if yaml_output is None:
+            print("Creating config file at {ef}".format(ef=FileHelper.GetEffectiveRoot() + 'etc/wpkgman.cfg'))
+            content = """# wpkgman master config file
+# edit carefully, if the YAML formatting breaks wpkgman will error
+
+options:
+    - color: true
+    - noconfirm: false # only enable this if you hate yourself
+
+mirrors:
+    mirror:
+        - priority: 0
+        - address: http://sundwarf.me/pkg/ # Will autocomplete $repo/$arch/$pkg
+
+repos:
+    stable:
+        loc: var/wpkgman/repos/stable.yml
+        priority: -1
+    testing:
+        loc: var/wpkgman/repos/testing.yml
+        priority: 0
+    extra:
+        loc: var/wpkgman/repos/extra.yml
+        priority: -1
+
+
+ignore_package:
+    - dummypackage # added to make YAML not complain
+    # Add packages you wish to ignore here
+    # - gcc
+
+upgrade_first:
+    # if any of these packages are installed, and have an update, will upgrade before anything else
+    - dummypackage
+    - wpkgman
+    - wsystem
+"""
+            f = FileHelper.OpenFileForWritingText('etc/wpkgman.yml')
+            f.write(content)
+        else:
+            self.options = (yaml_output['options'][0], yaml_output['options'][1])
+            self.mirrors = yaml_output['mirrors']
+            self.repos = yaml_output['repos']
+            self.ignore_package = yaml_output['dummy_package']
+            self.upgrade_first = yaml_output['upgrade_first']
