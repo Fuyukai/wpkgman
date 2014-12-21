e80ad653e1af21dac7ddc7ef48dbb2501f37cf1c
SunDwarf <eyesismine@gmail.com>
2014-12-21 18:42:39 +0000

    Add colours to WPKGMANINFO

--- pkgman/WPKGMANINFO.py
+++ pkgman/WPKGMANINFO.py
@@ -1,2 +1,10 @@
-__WPKGMAN_VERSION = "0.1.0"
+__WPKGMAN_VERSION = "0.1.2"
 __WPKGMAN_AUTHOR = "Eyes"
+
+class Color:
+    off = "\033[0m"
+    black = "\033[0;30m"
+    red = "\033[0;31m"
+    green = "\033[0;32m"
+    cyan = "\033[0;36m"
+    yellow = "\033[1;33m"
