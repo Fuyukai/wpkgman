589d5d1c2bad29b8a32c4022ffa6a790be025743
SunDwarf <eyesismine@gmail.com>
2014-12-23 12:11:48 +0000

    Change progress bar to support auto units

--- pkgman/ProgressBar.py
+++ pkgman/ProgressBar.py
@@ -1,7 +1,9 @@
 import sys
+
 import requests
 from progressbar import Bar, FileTransferSpeed, Percentage, ProgressBar
 from .WPKGMANINFO import Color as Color
+
 # Taken from http://stackoverflow.com/questions/10525635/python-3-and-requests-with-a-progressbar
 def get_file(file_url, name):
     r = requests.get(file_url, stream=True)
@@ -12,15 +14,38 @@ def get_file(file_url, name):
     bytes = 0
     widgets = [name, ": ", Bar(marker="=", left="[", right="] "),
         Percentage(), " ",  FileTransferSpeed(), " ",
-        str(round(bytes / 1024, 2)), "KB"
-        " of {0}KB".format(str(round(size / 1024, 2)))]
+        str(round(bytes / 1024, 2)), "KB",
+        " of {0}{1}".format(get_appropriate_rounded_size(size), get_appropriate_rounded_unit(size))]
     pbar = ProgressBar(widgets=widgets, maxval=size, fd=sys.stdout).start()
     file = []
     for buf in r.iter_content(1024):
         if buf:
             file.append(buf)
             bytes += len(buf)
-            pbar.widgets[7] = str(round(bytes / 1024, 2))
+            pbar.widgets[7] = get_appropriate_rounded_size(bytes)
+            pbar.widgets[8] = get_appropriate_rounded_unit(bytes)
             pbar.update(bytes)
     pbar.finish()
     return b"".join(file)
+
+
+def get_appropriate_rounded_size(bytes):
+    if 1024 < bytes < 1048576:
+        return str(round(bytes / 1024))
+    elif 1048576 < bytes < 1073741824:
+        return str(round(bytes / 1024 / 1024))
+    elif bytes > 1073741824:
+        return str(round(bytes / 1024 / 1024 / 1024))
+    else:
+        return str(round(bytes, 2))
+
+
+def get_appropriate_rounded_unit(bytes):
+    if 1024 < bytes < 1048576:
+        return 'KB'
+    elif 1048576 < bytes < 1073741824:
+        return 'MB'
+    elif bytes > 1073741824:
+        return 'GB'
+    else:
+        return 'B'
