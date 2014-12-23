e08e9c6d93d89bce75de3900572ef14606008e80
SunDwarf <eyesismine@gmail.com>
2014-12-23 12:32:35 +0000

    Add dirty hacks to WPKGMANINFO in case I ever need them again

--- pkgman/WPKGMANINFO.py
+++ pkgman/WPKGMANINFO.py
@@ -9,3 +9,10 @@ class Color:
     cyan = "\033[0;36m"
     yellow = "\033[1;33m"
     white = "\033[1;37m"
+
+    # Dirty hacks
+    # def remove(*args, **kwargs):
+    # print("os.remove(" + str(args) + str(kwargs) + ")")
+    # os.oldremove(*args, **kwargs)
+    # os.oldremove = os.remove
+    # os.remove = remove
